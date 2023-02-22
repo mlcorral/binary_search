656c072602accaf7278183f5300e7763c7f95822
MYA <corralmya@gmail.com>
2023-02-21 19:13:18 -0800

    extra credit fail :(

--- binary_search.py
+++ binary_search.py
@@ -1,69 +1,57 @@
 #!/bin/python3:
+
+
 def find_smallest_positive(xs):
-    if len(xs) == 0:
+    if not xs:
         return None
-    
+
     def go(left, right):
         if xs[left] > 0:
             return left
         if left == right:
             return None
-        
-        mid = (left + right) // 2
-        
+
+        mid = (left + right) >> 1
+
         if xs[mid] > 0 and (mid == left or xs[mid-1] <= 0):
             return mid
-        
+
         if xs[mid] <= 0:
             left = mid + 1
         else:
             right = mid - 1
-        
+
         return go(left, right)
-    
+
     return go(0, len(xs) - 1)
 
-def count_repeats(xs, x):
-    def find_first(xs, x):
-        left, right = 0, len(xs)
-        while left < right:
-            mid = (left + right) // 2
-            if xs[mid] < x:
-                right = mid
-            else:
-                left = mid + 1
-        return left
 
-    def find_last(xs, x):
-        left, right = 0, len(xs)
-        while left < right:
-            mid = (left + right) // 2
-            if xs[mid] >= x:
-                left = mid + 1
-            else:
-                right = mid
-        return left
+def count_repeats(arr, n):
+    count = 0
+    for i in arr:
+        if i == n:
+            count += 1
+    return count
 
-    return find_last(xs, x) - find_first(xs, x)
 
 def argmin(f, lo, hi, epsilon=1e-3):
     while hi - lo > epsilon:
-        mid1 = lo + (hi - lo) / 3
-        mid2 = hi - (hi - lo) / 3
+        mid1 = lo + ((hi - lo) / 3)
+        mid2 = hi - ((hi - lo) / 3)
         if f(mid1) < f(mid2):
             hi = mid2
         else:
             lo = mid1
-    return (lo + hi) / 2
+    return ((lo + hi) / 2)
+
 
 def find_boundaries(f):
     lo, hi = -1, 1
     while f(hi) < f(lo):
-        lo, hi = hi, 2 * hi
+        lo, hi = hi, (hi << 1)
     return lo, hi
+
 
 def argmin_simple(f, epsilon=1e-3):
     lo, hi = find_boundaries(f)
     return argmin(f, lo, hi, epsilon)
-
-
