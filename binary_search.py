091d6f4bf44eb5625c1b65c47a927dd610b8b708
MYA <corralmya@gmail.com>
2023-02-21 22:36:39 -0800

    done hopefully

--- binary_search.py
+++ binary_search.py
@@ -1,6 +1,4 @@
 #!/bin/python3:
-
-
 def find_smallest_positive(xs):
     if not xs:
         return None
@@ -11,7 +9,7 @@ def find_smallest_positive(xs):
         if left == right:
             return None
 
-        mid = (left + right) >> 1
+        mid = (left + right) // 2
 
         if xs[mid] > 0 and (mid == left or xs[mid - 1] <= 0):
             return mid
@@ -26,25 +24,70 @@ def find_smallest_positive(xs):
     return go(0, len(xs) - 1)
 
 
-def count_repeats(arr, n):
-    if not arr:
+def count_repeats(xs, x):
+    if len(xs) == 0:
         return 0
 
-    if arr[0] == n:
-        return 1 + count_repeats(arr[1:], n)
+    def go(left, right):
+        if left == right:
+            if xs[left] == x:
+                return left
+            else:
+                return None
+        result = None
+        mid = (left + right) // 2
+        if xs[mid] == x:
+            result = mid
+            if xs[mid - 1] == x and mid != 0:
+                right = mid - 1
+            else:
+                return result
+        elif x < xs[mid]:
+            left = mid + 1
+        else:
+            right = mid - 1
+        return go(left, right)
+
+    a = go(0, len(xs)-1)
+
+    def go2(left,right):
+        if left == right:
+            if xs[left] == x:
+                return left
+            else:
+                return None
+        result = None
+        mid = (left + right) // 2
+        if xs[mid] == x: 
+            result = mid
+            if xs[mid + 1] == x and (mid + 1) < len(xs):
+                left = mid + 1
+            else:
+                return result
+        elif x > xs[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+        return go2(left, right)
+
+    a2 = go2(0, len(xs) - 1)
+
+    if a is not None and a2 is not None:
+        return (a2 - a) + 1
     else:
-        return count_repeats(arr[1:], n)
+        return 0
 
 
 def argmin(f, lo, hi, epsilon=1e-3):
-    while hi - lo > epsilon:
+    if hi - lo <= epsilon:
+        return (lo + hi) / 2
+    else:
         mid1 = lo + ((hi - lo) / 3)
         mid2 = hi - ((hi - lo) / 3)
         if f(mid1) < f(mid2):
-            hi = mid2
+            return argmin(f, lo, mid2, epsilon)
         else:
-            lo = mid1
-    return ((lo + hi) / 2)
+            return argmin(f, mid1, hi, epsilon)
 
 
 def find_boundaries(f):
@@ -57,3 +100,4 @@ def find_boundaries(f):
 def argmin_simple(f, epsilon=1e-3):
     lo, hi = find_boundaries(f)
     return argmin(f, lo, hi, epsilon)
+
